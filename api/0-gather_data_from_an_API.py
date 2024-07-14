65444920b28ee135d7eac02b7794fc1c3ee1aec0
LJThao <yengfong.thao@atlasschool.com>
2024-07-13 21:21:56 -0500

    pep8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,21 +4,22 @@ API, for a given employee ID, returns
 information about his/her TODO list"""
 
 
-import requests  # Importing from requests module so it can handle HTTP requests
+import requests  # Importing from requests module so it can handle HTTP request
 import sys  # Importing from sys module so it can handle command-line arguments
 
 
 def todo_list(employee_id):
     """Retrieving the TODO list for an employee and returns the information"""
     # Retrieve the employee's data from the API and then converts it to JSON
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-                        ).json()
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/"
+                        f"{employee_id}").json()
     # Retrieve the TODO list data for the employee and then converts it to JSON
-    todos = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-                         ).json()
+    todos = requests.get(f"https://jsonplaceholder.typicode.com/"
+                         f"todos?userId={employee_id}").json()
 
     # Print the employee's TODO list in this format
-    print(f"Employee {user['name']} is done with tasks({sum(task['completed'] for task in todos)}/{len(todos)}):")
+    print(f"Employee {user['name']} is done with tasks("
+          f"{sum(task['completed'] for task in todos)}/{len(todos)}):")
     # Loop through the tasks and then print the titles of the completed tasks
     for task in todos:
         if task['completed']:  # Checks if the task is completed
